d3fdfe54ce67e82928dfd7cf9e1d4885d3411cab
Maha Magdy <mahamagdy3@gmail.com>
2021-05-12 00:11:20 +0200

    Make user_params method private one

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,8 @@ class UsersController < ApplicationController
       render(:edit)
     end
   end
-
+  
+  private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
