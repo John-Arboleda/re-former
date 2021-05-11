fb110cab1088ced8a2786b3ce5e1203addf6acaa
Maha Magdy <mahamagdy3@gmail.com>
2021-05-12 00:19:56 +0200

    Work on the code review comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,8 +27,9 @@ class UsersController < ApplicationController
       render(:edit)
     end
   end
-  
+
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
+                                              message: 'need to enter a valid email' }
   validates :password, presence: true
 end
